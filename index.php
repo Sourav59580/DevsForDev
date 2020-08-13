29dcaadc97ec80c733a59ab5df480ba43a1ddaf2
sourav santra <souravsantra59580@gmail.com>
2020-08-13 21:27:21 +0530

    second commit

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
     <title>Document</title>
     <link rel="stylesheet" href="./style.css">
     <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="./css/Navbar.css">
+    <link rel="stylesheet" href="./css/RightsideNavbar.css">
+    <link rel="stylesheet" href="./css/LeftSideNavbar.css">
+    <link rel="stylesheet" href="./css/MainBody.css">
+    <link rel="stylesheet" href="./css/header.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Risque&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -21,34 +26,41 @@
         <!--Navbar page -->
         <?php include_once("./pages/Navbar.php") ?>
     </div>
-    <!-- Signup modal -->
-<div class="modal fade" id="register" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-body">
-                <h4>Register Now</h4>
-                <hr>
+    <!-- end Navbar coding -->
+
+    <!--start header coding -->
+    <div class="container-fluid header">
+        <div class="row h-100">
+            <div class="col-md-6 d-flex align-items-center p-4 mb-4">
+                <div>
+                    <h3 class="text-light mb-4 font-weight-bold">Share & grow the world's knowledge!</h3>
+                    <h5 class="text-light" style="line-height: 25px;letter-spacing:1px">We want to connect the people who have knowledge to the people who need it, to bring together people with different perspectives so they can understand each other better, and to empower everyone to share their knowledge.</h5>
+                </div>
+
+            </div>
+            <div class="col-md-6 d-flex align-items-center justify-content-center mb-4">
+                <button class="btn btn-primary px-4">Create A New Account</button>
             </div>
         </div>
     </div>
+    <!--end header coding -->
 
-</div>
-    <!-- end Navbar coding -->
+
+
+
+
     <!-- start Body Coding-->
     <div class="container-fluid px-0 main-container">
-
         <!-- Start Left Sidenavbar Coding -->
         <div class="float-left sidenavbar">
 
         </div>
         <!--End Left Sidebar coding -->
-
         <!--Start Other Part Coding-->
         <div class="container-fluid px-0">
             <div class="row">
                 <!-- Start Main body Coding -->
                 <div class="col-lg-9 bg-white px-0" style="height: 100vh;">
-
                     <!--Start body Navbar Coding -->
                     <div class="container-fluid bg-white border-bottom" style="height: 60px;">
                         <button class="btn sidenavbar-toggler d-lg-none"><i class="fa fa-bars"></i></button>
@@ -65,10 +77,8 @@
 
                 <!-- Start Right Sidebar coding-->
                 <div class="col-lg-3 right-sidenavbar">
-
                     <!--Right Sidebar page-->
                     <?php include("./pages/RightNavbar.php")  ?>
-                    
                 </div>
                 <!-- End Right Sidebar coding-->
             </div>
--- pages/RightNavbar.php
+++ pages/RightNavbar.php
@@ -1,25 +1,60 @@
-<div class="container bg-white shadow-sm py-2">
+<div class="container bg-light shadow-sm py-2">
     <div class="">
         <button class="btn btn-block btn-primary">Ask a Question</button>
         <hr>
     </div>
 
+    <!-- second row --> 
+    <div class="container bg-white shadow-sm mb-4">
+        <div class="row">
+            <div class="col-6 border py-2 px-0">
+                <div style="border-left:3px solid #2D6FF7;" class="d-flex flex-column justify-content-center align-items-center">
+                    <p class="text-primary">Questions</p>
+                    <h4>21</h4>
+                </div>
+            </div>
+            <div class="col-6 border py-2 px-0">
+                <div style="border-right:3px solid #2D6FF7;" class="d-flex flex-column justify-content-center align-items-center">
+                    <p class="text-danger">Answers</p>
+                    <h4>21</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6 border py-2 px-0">
+                <div style="border-left:3px solid #2D6FF7;" class="d-flex flex-column justify-content-center align-items-center">
+                    <p class="text-success">Best Answers</p>
+                    <h4>21</h4>
+                </div>
+            </div>
+            <div class="col-6 border py-2 px-0">
+                <div style="border-right:3px solid #2D6FF7;" class="d-flex flex-column justify-content-center align-items-center">
+                    <p class="text-info">Users</p>
+                    <h4>21</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--third row -->
     <div class="bg-white shadow-sm p-3 border">
-        <h5>Login</h5>
+        <h4>Login</h4>
         <hr>
         <input type="text" name="username" placeholder="&#xf2bd; Username" class="form-control rounded border-0 bg-white shadow-sm mb-3">
         <input type="password" name="password" placeholder="&#xf023; Password" class="form-control rounded border-0 bg-white shadow-sm mb-3">
         <button class="btn btn-warning btn-block mb-3">Log in</button>
-        <input type="checkbox" checked class="mr-1"><small>Remember Me</small><a href="#staticBackdrop" class="text-dark ml-4" data-toggle="modal">Register</a>
+        <input type="checkbox" checked class="mr-1"><small>Remember Me</small><a href="#staticBackdrop" class="text-dark ml-4 register" data-toggle="modal">Register</a>
     </div>
-
-
+    
+    <!--fourth row tab -->
+    <div class="bg-light"></div>
 
 
 </div>
 
+
 <!-- Signup modal -->
-<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal fade" id="staticBackdrop">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-body">
